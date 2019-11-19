67da69c5f3b70a3083fde6a5a5bff26891ce44da
Alexandr Varygin <otz-job@mail.ru>
2019-11-19 20:10:30 +0300

    delete comment from index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 include 'functions.php';
-include 'database/start.php';
+$db = include 'database/start.php';
 $posts = $db->getAll();
-
-//$posts = getAllPosts($pdo);
-//$users = getAllusers($pdo);
-
-//4. Вывести через foreach
 include 'index.view.php';
 ?>
 
