f62c9889f999bb63c2930fe589382ecb8de568d0
Alexandr Varygin <otz-job@mail.ru>
2019-11-28 20:26:40 +0300

    example - how we can use router URL?

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
 include 'functions.php';
-$db = include 'database/start.php';
 
-$posts = $db->getAll('posts');
-include 'index.view.php';
+// Идея использования маршрутизатора | router для редиректа запросов
+
+//dd($_SERVER);
+
+$routes = [
+    "/" => 'functions/homepage.php',
+    "/about" => 'functions/about.php'
+];
+
+$route = $_SERVER['REQUEST_URI'];
+
+if(array_key_exists($route, $routes)) {
+    include $routes[$route];
+    exit;
+} else {
+    dd(404);
+}
+
+//$db = include 'database/start.php';
+//
+//$posts = $db->getAll('posts');
+//include 'index.view.php';
 ?>
